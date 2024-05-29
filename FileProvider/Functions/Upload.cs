25663a8036165076d66a2610a8331dfaed1579a0
Nils0009 <143409567+Nils0009@users.noreply.github.com>
2024-05-29 09:28:56 +0200

    FileProvider done with Upload function and FileService

--- FileProvider/Functions/Upload.cs
+++ FileProvider/Functions/Upload.cs
@@ -1,3 +1,5 @@
+using FileProvider.Infrastructure.Entities;
+using FileProvider.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -5,13 +7,42 @@ using Microsoft.Extensions.Logging;
 
 namespace FileProvider.Functions;
 
-public class Upload(ILogger<Upload> logger)
+public class Upload(ILogger<Upload> logger, FileService fileService)
 {
+
     private readonly ILogger<Upload> _logger = logger;
+    private readonly FileService _fileService = fileService;
 
     [Function("Upload")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
     {
-        
+		try
+		{
+            if (req.Form.Files["file"] is IFormFile file)
+            {
+                var containerName = !string.IsNullOrEmpty(req.Query["containerName"]) ? req.Query["containerName"].ToString() : "blazorapp-images";
+
+                var fileEntity = new FileEntity
+                {
+                    FileName = _fileService.SetFileName(file),
+                    ContentType = file.ContentType,
+                    ContainerName = containerName
+                };
+
+                await _fileService.SetBlobContainerAsync(fileEntity.ContainerName);
+                var filePath = await _fileService.UploadFileAsync(file, fileEntity);
+                fileEntity.FilePath = filePath;
+
+                await _fileService.SaveToDatabaseAsync(fileEntity);
+                return new OkObjectResult(fileEntity);
+            }
+		}
+		catch (Exception ex)
+		{
+            _logger.LogError(ex, ex.Message);
+		}
+
+        return new BadRequestResult();
     }
 }
--- FileProvider/Services/FileService.cs
+++ FileProvider/Services/FileService.cs
@@ -1,7 +1,9 @@
 ﻿using Azure.Storage.Blobs;
 using FileProvider.Infrastructure.Contexts;
 using Microsoft.Extensions.Logging;
-
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Http;
+using FileProvider.Infrastructure.Entities;
 namespace FileProvider.Services;
 
 public class FileService(ILogger<FileService> logger, DataContext dataContext, BlobServiceClient blobServiceClient)
@@ -9,11 +11,38 @@ public class FileService(ILogger<FileService> logger, DataContext dataContext, B
     private readonly ILogger<FileService> _logger = logger;
     private readonly DataContext _dataContext = dataContext;
     private readonly BlobServiceClient _blobServiceClient = blobServiceClient;
+    private BlobContainerClient _containerClient;
 
 
     public async Task SetBlobContainerAsync(string containerName)
     {
         var container = _blobServiceClient.GetBlobContainerClient(containerName);
         await container.CreateIfNotExistsAsync();
+    }
+
+    public string SetFileName(IFormFile file)
+    {
+        var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+        return fileName ;
+    }
+
+    public async Task<string> UploadFileAsync(IFormFile file, FileEntity fileEntity)
+    {
+        BlobHttpHeaders headers = new BlobHttpHeaders()
+        {
+            ContentType = file.ContentType
+        };
+
+        var blobClient = _containerClient.GetBlobClient(fileEntity.FileName);
+        using var stream = file.OpenReadStream();
+        await blobClient.UploadAsync(stream, headers);
+
+        return blobClient.Uri.ToString();
+    }
+
+    public async Task SaveToDatabaseAsync(FileEntity fileEntity)
+    {
+        _dataContext.Add(fileEntity);
+        await _dataContext.SaveChangesAsync();
     }
 }
